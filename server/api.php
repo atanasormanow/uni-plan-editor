bbd605ccf380f0aac0dd7f723cdb1da7f3070084
Atanas Ormanov <atanasormanow@gmail.com>
2023-02-04 14:08:26 +0200

    Add endpoint for getting migration by name

--- server/api.php
+++ server/api.php
@@ -8,9 +8,8 @@ $password = "";
 $db = new mysqli($servername, $username, $password);
 
 if (!$db) {
-    die("Connection failed: " . mysqli_connect_error());
+  die("Connection failed: " . mysqli_connect_error());
 }
-echo "Connected successfully!";
 
 switch ($_SERVER['REQUEST_METHOD']) {
   case 'GET':
@@ -32,38 +31,54 @@ switch ($_SERVER['REQUEST_METHOD']) {
 }
 
 // Handle GET request to receive a list of the databases
-// TODO: handle other get requests
-function handleGetRequest() {
+function handleGetRequest()
+{
   global $db;
-  $result = $db->query("SHOW DATABASES");
-  $databases = array();
-  while ($row = $result->fetch_assoc()) {
-    $databases[] = $row['Database'];
+  $data = json_decode(file_get_contents("php://input"), true);
+  // get migration by name
+  if (isset($data['migration'])) {
+    // escape to avoid SQL injection
+    $name = $db->real_escape_string($data['migration']);
+    $db->query("USE migration_manager");
+    $migration = $db->query("SELECT * FROM migrations WHERE name='$name'")->fetch_assoc();
+    echo json_encode($migration);
+  // list all databases
+  } else {
+    $result = $db->query("SHOW DATABASES");
+    $databases = array();
+    while ($row = $result->fetch_assoc()) {
+      $databases[] = $row['Database'];
+    }
+    echo json_encode($databases);
   }
-  echo json_encode($databases);
 }
 
 // Handle POST request to create a new database
-function handlePostRequest() {
+// TODO: create migration
+function handlePostRequest()
+{
   global $db, $username;
   $data = json_decode(file_get_contents("php://input"), true);
-  // prefix database with username as it has full access to such tables
-  $db->query("CREATE DATABASE " . $username . "_" . $data['name']);
+  $name = $db->real_escape_string($data['name']);
+  // prefix database with username to have access rights
+  $db->query("CREATE DATABASE " . $username . "_$name");
   echo "Database created";
 }
 
 // Handle PUT request to execute a given SQL query on a particular database
-// WIP: should use query template that is stored in a .json
-function handlePutRequest() {
+// TODO: should be able to run up/down for some migration
+function handlePutRequest()
+{
   global $db;
-  $data = json_decode(file_get_contents("php://input"), true);
-  $db->query("USE " . $data->database);
-  $db->query($data->query);
+  // $data = json_decode(file_get_contents("php://input"), true);
+  // $db->query("USE " . $data->database);
+  // $db->query($data->query);
   echo "Query executed";
 }
 
 // Handle DELETE request to delete a database
-function handleDeleteRequest() {
+function handleDeleteRequest()
+{
   global $db, $username;
   $data = json_decode(file_get_contents("php://input"), true);
   $db->query("DROP DATABASE " . $username . "_" . $data['name']);
