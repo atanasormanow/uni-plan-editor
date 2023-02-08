861125cd18a031ea0f659f21e30618d38711da26
Atanas Ormanov <atanasormanow@gmail.com>
2023-02-08 17:19:50 +0200

    Add the actual config

---
+++ server/config.php
@@ -0,0 +1,9 @@
+<?php
+class Config
+{
+  public static $host = "localhost";
+  public static $servername = "localhost";
+  public static $username = "webcourse";
+  public static $password = "webtech2223";
+  public static $type = "mysql";
+}
