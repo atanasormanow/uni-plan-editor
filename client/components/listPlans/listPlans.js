22491c951e09d849238ba3fe08da32413fb7d77c
Atanas Ormanov <atanasormanow@gmail.com>
2023-05-15 23:51:48 +0300

    refactor(listPlans): abstract cell creation

--- client/components/listPlans/listPlans.js
+++ client/components/listPlans/listPlans.js
@@ -2,37 +2,33 @@ window.onload = function() {
   document
     .getElementById("create-plan-button")
     .addEventListener('click', (_event) => {
-      window.location.replace('http://localhost/myapp/client/components/createPlan/createPlan.html');
+      window.location.replace(CLIENT_COMPONENTS + 'createPlan/createPlan.html');
     });
 
   fetch(SERVER_CONTROLLERS + 'get_all_plans.php')
     .then(response => response.json())
-    .then(data => displayPlans(JSON.parse(data.data)))
+    .then(({ data }) => displayPlans(JSON.parse(data)))
     .catch(error => console.error('Error:', error));
 };
+
+function addColumn(toRow, textContent, className) {
+  const column = document.createElement('td');
+  column.className = className;
+  column.textContent = textContent;
+  toRow.appendChild(column);
+}
 
 function displayPlans(plans) {
   const plansList = document.getElementById('plans-list');
 
-  plans.forEach((plan) => {
+  plans.forEach(plan => {
     const row = document.createElement('tr');
+    const columnApender = addColumn.bind(this, row);
 
-    const idCell = document.createElement('td');
-    idCell.textContent = plan.id;
-    row.appendChild(idCell);
-
-    const nameCell = document.createElement('td');
-    nameCell.className = 'plan-name';
-    nameCell.textContent = plan.name;
-    row.appendChild(nameCell);
-
-    const descriptionCell = document.createElement('td');
-    descriptionCell.textContent = plan.description;
-    row.appendChild(descriptionCell);
-
-    const ownerCell = document.createElement('td');
-    ownerCell.textContent = plan.owner;
-    row.appendChild(ownerCell);
+    columnApender(plan.id);
+    columnApender(plan.name, 'plan-name');
+    columnApender(plan.description);
+    columnApender(plan.owner);
 
     const viewCell = document.createElement('td');
     const anchor = document.createElement('a');
