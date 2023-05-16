9581904434b2921115ef5159ab27804925768f76
Atanas Ormanov <atanasormanow@gmail.com>
2023-05-16 14:43:11 +0300

    Extend plan model

--- server/model/plan.php
+++ server/model/plan.php
@@ -6,9 +6,23 @@ require('../fpdf/fpdf.php');
 class Plan
 {
   private $planId;
+
+  // Избираема/Задължителна
+  private $type;
+  private $targetMajors;
   private $name;
-  private $description;
+  private $department;
   private $owner;
+  private $busyness;
+  private $credits;
+  private $description;
+  private $requiredSkills;
+  //Other plans
+  private $dependencies;
+  private $learnedSkills;
+  private $contents;
+  private $examSynopsis;
+  private $bibliography;
 
   public function __construct($planId, $name, $description, $owner_id)
   {
@@ -74,5 +88,115 @@ class Plan
   public function setDescription($description)
   {
     $this->description = $description;
+  }
+
+  public function getType()
+  {
+    return $this->type;
+  }
+
+  public function setType($type)
+  {
+    $this->type = $type;
+  }
+
+  public function getTargetMajors()
+  {
+    return $this->targetMajors;
+  }
+
+  public function setTargetMajors($targetMajors)
+  {
+    $this->targetMajors = $targetMajors;
+  }
+
+  public function getDepartment()
+  {
+    return $this->department;
+  }
+
+  public function setDepartment($department)
+  {
+    $this->department = $department;
+  }
+
+  public function getBusyness()
+  {
+    return $this->busyness;
+  }
+
+  public function setBusyness($busyness)
+  {
+    $this->busyness = $busyness;
+  }
+
+  public function getCredits()
+  {
+    return $this->credits;
+  }
+
+  public function setCredits($credits)
+  {
+    $this->credits = $credits;
+  }
+
+  public function getRequiredSkills()
+  {
+    return $this->requiredSkills;
+  }
+
+  public function setRequiredSkills($requiredSkills)
+  {
+    $this->requiredSkills = $requiredSkills;
+  }
+
+  public function getDependencies()
+  {
+    return $this->dependencies;
+  }
+
+  public function setDependencies($dependencies)
+  {
+    $this->dependencies = $dependencies;
+  }
+
+  public function getLearnedSkills()
+  {
+    return $this->learnedSkills;
+  }
+
+  public function setLearnedSkills($learnedSkills)
+  {
+    $this->learnedSkills = $learnedSkills;
+  }
+
+  public function getContents()
+  {
+    return $this->contents;
+  }
+
+  public function setContents($contents)
+  {
+    $this->contents = $contents;
+  }
+
+  public function getExamSynopsis()
+  {
+    return $this->examSynopsis;
+  }
+
+  public function setExamSynopsis($examSynopsis)
+  {
+    $this->examSynopsis = $examSynopsis;
+  }
+
+  public function getBibliography()
+  {
+    return $this->bibliography;
+  }
+
+  public function setBibliography($bibliography)
+  {
+    $this->bibliography = $bibliography;
   }
 }
